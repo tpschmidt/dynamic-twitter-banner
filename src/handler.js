9974e28f664cc334c2d7bee3d8b995f1a46b22e2
Tobias Schmidt <tobias.schmidt@ic-consult.de>
2021-09-04 13:15:53 +0200

    update banner & remove headline

--- src/handler.js
+++ src/handler.js
@@ -10,6 +10,11 @@ const widthHeightFollowerImage = 90
 const bannerFileName = '1500x500.png'
 const maskFileName = 'mask.png'
 
+const followerPosition = {
+    x: 1200,
+    y: 400
+}
+
 function getVariable(name) {
     if (fs.existsSync(`${__dirname}/../creds.json`)) {
         return require(`${__dirname}/../creds.json`)[name]
@@ -47,35 +52,33 @@ async function getImagesOfLatestFollowers() {
         .map((user, index) => saveAvatar(user, `/tmp/${index}.png`)))
 }
 
-async function createBanner(headline) {
+async function createBanner() {
     const banner = await Jimp.read(`${__dirname}/../assets/${bannerFileName}`)
     const mask = await Jimp.read(`${__dirname}/../assets/${maskFileName}`)
-    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
+    // const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
     // build banner
     console.log(`Adding followers...`)
     await Promise.all([...Array(numberOfFollowers)].map((_, i) => {
         return new Promise(async resolve => {
             const image = await Jimp.read(`/tmp/${i}.png`)
-            const x = 600 + i * (widthHeightFollowerImage + 10);
+            const x = followerPosition.x + i * (widthHeightFollowerImage + 10);
             console.log(`Appending image ${i} with x=${x}`)
-            banner.composite(image.mask(mask, 0, 0), x, 350);
+            banner.composite(image.mask(mask, 0, 0), x, followerPosition.y);
             resolve()
         })
     }))
-    console.log(`Adding headline...`)
-    banner.print(font, 180, 65, headline);
     await banner.writeAsync('/tmp/1500x500_final.png');
 }
 
-async function getLatestArticleHeadline() {
-    console.log(`Retrieving headline...`)
-    const rss = await Feed.load(`https://medium.com/feed/@${getVariable('MEDIUM_HANDLE')}`)
-    const title = rss.items[0].title
-    console.log(`Retrieved headline: ${title}`)
-    // add padding left & right to align it properly
-    const padding = ' '.repeat(Math.ceil((80 - title.length) / 2))
-    return `${padding}${title}${padding}`;
-}
+// async function getLatestArticleHeadline() {
+//     console.log(`Retrieving headline...`)
+//     const rss = await Feed.load(`https://medium.com/feed/@${getVariable('MEDIUM_HANDLE')}`)
+//     const title = rss.items[0].title
+//     console.log(`Retrieved headline: ${title}`)
+//     // add padding left & right to align it properly
+//     const padding = ' '.repeat(Math.ceil((80 - title.length) / 2))
+//     return `${padding}${title}${padding}`;
+// }
 
 async function uploadBanner() {
     console.log(`Uploading to twitter...`)
@@ -86,8 +89,7 @@ async function uploadBanner() {
 
 module.exports.handler = async () => {
     await getImagesOfLatestFollowers()
-    const title = await getLatestArticleHeadline()
-    await createBanner(title)
+    await createBanner()
     await uploadBanner()
 }
 
